cd7692c0398b2c1448211d7c96b53adc5ab17bcc
Squareof3 <haruharatakun@gmail.com>
2017-08-24 22:04:40 -0400

    final draft

---
+++ ClozeCard.js
@@ -0,0 +1,10 @@
+function ClozeCard(text, cloze) {
+    this.fullText = text;
+    this.cloze = cloze;
+    this.partial = text.replace(cloze, "...");
+}
+
+function ClozeCardPrototype() {
+    if (err) throw err;
+}
+module.exports = ClozeCards;
